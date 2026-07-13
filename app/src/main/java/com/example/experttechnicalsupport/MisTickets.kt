2504d765bc2fa1885de6ad116c3b2653a8983173
isauSuarez <2124100174@soy.utj.edu.mx>
2026-07-12 20:27:30 -0600

    Mis tickets comentarios

--- app/src/main/java/com/example/experttechnicalsupport/MisTickets.kt
+++ app/src/main/java/com/example/experttechnicalsupport/MisTickets.kt
@@ -16,5 +16,5 @@ class MisTickets : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-}
+    }//on create
+}//class
